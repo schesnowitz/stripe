c6a31b3a0592522accd3d68f512bb3af7fe5aa61
Steve Chesnowitz <steve@chesnowitz.com>
2018-02-16 00:10:14 -0500

    working stripe

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_02_15_190836) do
     t.string "encrypted_password", default: "", null: false
     t.string "stripe_subscription_id"
     t.string "stripe_id"
+    t.boolean "admin"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
