03412c4e55c9ae9e5e91843e39e12e21cf0735c9
Steve Chesnowitz <steve@chesnowitz.com>
2018-02-15 23:58:56 -0500

    working stripe

--- db/migrate/20180215190836_devise_create_users.rb
+++ db/migrate/20180215190836_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
       t.string :stripe_subscription_id
       t.string :stripe_id
+      t.boolean :admin
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
